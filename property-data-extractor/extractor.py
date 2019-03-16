f3551442b73e59e981420ac733081f2741c8d778
mkrul <mishakrul@gmail.com>
2019-03-16 18:44:12 -0400

    add gitignore file

--- property-data-extractor/extractor.py
+++ property-data-extractor/extractor.py
@@ -1,3 +1,4 @@
+from pymongo import MongoClient
 import requests
 import json
 
@@ -12,5 +13,11 @@ data = {
 response = requests.post("https://property.spatialest.com/nc/durham/data/propertycard", data=data)
 pretty_json = json.loads(response.content)
 
-
 print(json.dumps(pretty_json, indent=2))
+
+file = open("password.txt", "r")
+password = file.read()
+
+db_endpoint = f"mongodb+srv://mkrul:{password}@cluster0-uadok.mongodb.net/test?retryWrites=true"
+client = MongoClient("mongodb+srv://mkrul:{password}@cluster0-uadok.mongodb.net/test?retryWrites=true")
+
